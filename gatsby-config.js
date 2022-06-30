e948a1248809c78388ac8327d4030726adc6babc
dunky11 <timv94@zedat.fu-berlin.de>
2022-07-01 01:17:09 +0200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `VoiceSmith Documentation`,
+    defaultTitle: `VoiceSmith Documentation`,
+    siteTitleShort: `VoiceSmith Documentation`,
+    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
+    siteUrl: `https://rocketdocs.netlify.app`,
+    siteAuthor: `@dunky11`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        yamlFilesPath: `src/yamlFiles`,
+        repositoryUrl: `https://github.com/jpedroschmitz/rocketdocs`,
+        baseDir: `examples/gatsby-theme-docs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocket Docs`,
+        short_name: `Rocket Docs`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.app`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Logo(props) {
+  return (
+    <h1 >VoiceSmith</h1>
+  );
+} 
